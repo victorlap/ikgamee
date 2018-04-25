a36ef9ceea5a06d501a995cec2f337b16edd6624
Victor Lap <victorlap@outlook.com>
2018-04-25 22:03:26 +0200

    Change session configuration

--- config/session.php
+++ config/session.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => 365 * 24 * 60,
 
     'expire_on_close' => false,
 
@@ -124,7 +124,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_session'
+        'ikgamee_session'
     ),
 
     /*
